2e53a76ed6f1c6152d793a79c91e1b2d601bb336
IadosVicelord <IadosVicelord@github.com>
2019-12-10 16:35:46 +0400

    Update GeneralDataModel.cs

--- ChatClient/Models/GeneralDataModel.cs
+++ ChatClient/Models/GeneralDataModel.cs
@@ -18,6 +18,10 @@ namespace Client.Models
         ChatUser SelectedUser;
         #endregion
 
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="Shell">Основной экран-контейнер</param>
         public GeneralDataModel(ShellViewModel Shell)
         {
             //Поля для работы с сервисом
@@ -28,14 +32,20 @@ namespace Client.Models
             this.Shell = Shell;
         }
 
-        //Подключение пользователя по логину и паролю
+        /// <summary>
+        /// Подключение пользователя по логину и паролю
+        /// </summary>
+        /// <param name="username">Имя пользователя</param>
+        /// <param name="password">Пароль</param>
         public void Connect(string username, string password)
         {
             CurrentUser = new ChatUser { ID = 0, Name = username };
             Server.Connect(CurrentUser, password);
         }
 
-        //Отключение пользователя
+        /// <summary>
+        /// Отключение пользователя
+        /// </summary>
         public void Disconnect()
         {
             if (CurrentUser != null)
@@ -46,22 +56,32 @@ namespace Client.Models
             }
         }
 
-        //Обновление ссылки на текущего пользователя при успешной авторизации
+        /// <summary>
+        /// Обновление ссылки на текущего пользователя при успешной авторизации
+        /// </summary>
         public void UpdateCurrentUser()
         {
             Shell.CurrentUser = CurrentUser;
             Shell.IsUserLogged = true;
         }
 
-        //Обновление списка доступных пользователей
+        /// <summary>
+        /// Обновление списка подключенных пользователей
+        /// </summary>
+        /// <param name="users">Массив подключенных пользователей</param>
         public void UpdateConnectedUsers(ChatUser[] users)
         {
+            //Определить ID текущего пользователя
             CurrentUser.ID = users.FirstOrDefault(x => x.Name == CurrentUser.Name).ID;
+            //Обновить информацию о текущем пользователе
             UpdateCurrentUser();
+            //Удалить текущего пользователя из полученного списка
             users = users.Where(x => x.Name != CurrentUser.Name).ToArray();
 
+            //Обновить список подключенных пользователей
             Shell.ConnectedUsers = new Caliburn.Micro.BindableCollection<ChatUser>(users);
 
+            //Сортировка пользователей по дате последнего сообщения в переписке
             Shell.ConnectedUsers.Sort((a, b) => a.LastMessageDate.CompareTo(b.LastMessageDate));
         }
 
@@ -72,35 +92,54 @@ namespace Client.Models
         /// <param name="updatedTime">Новая дата последнего сообщения</param>
         public void UpdateConnectedUsers(int updatedID, DateTime updatedTime)
         {
-            var u = Shell.ConnectedUsers.FirstOrDefault(x => x.ID == updatedID);
+            //Обнаружение пользователя для которого предполагаются изменения
+            ChatUser u = Shell.ConnectedUsers.FirstOrDefault(x => x.ID == updatedID);
+            //Обновление информации о последнем сообщении пользователя
             u.LastMessageDate = updatedTime;
+            //Обновить информацию о наличии переписки с пользователем
             if (!u.IsMessageExist)
                 u.IsMessageExist = true;
 
+            //Сортировка пользователей по дате последнего сообщения переписки
             Shell.ConnectedUsers.Sort((a, b) => a.LastMessageDate.CompareTo(b.LastMessageDate));
             Shell.UpdateConnectedList();
         }
         
-        //Выбранный пользователь изменился
+        /// <summary>
+        /// Обработка обновления выбранного пользователя
+        /// </summary>
+        /// <param name="User">Выбранный пользователь</param>
         public void SetSelectedUser(ChatUser User)
         {
             if(User != null)
                 this.SelectedUser = User;
         }
 
-        //Получение идентификатора выбранного пользователя
+        /// <summary>
+        /// Получение идентификатора выбранного пользователя
+        /// </summary>
+        /// <returns>Идентификатор выбранного пользователя</returns>
         public int GetSelectedUserID() => SelectedUser.ID;
 
-        //Выбран ли хоть один пользователь
+        /// <summary>
+        /// Получение информации о том, выбран ли хоть один пользователь
+        /// </summary>
+        /// <returns>Флаг выбран ли пользователь</returns>
         public bool IsNoOneSelected() => SelectedUser == null ? true : false;
 
-        //Добавить сообщение в общий список
+        /// <summary>
+        /// Добавление сообщения в общий список
+        /// </summary>
+        /// <param name="msg">Сообщение</param>
         public void AddMessageToList(Message msg)
         {
             Shell.UpdateMessageList(msg);
         }
 
-        //Запрос истории переписки
+        /// <summary>
+        /// Запрос истории переписки
+        /// </summary>
+        /// <returns>Массив сообщений - история переписки</returns>
         public Message[] GetHistory()
         {
             Message[] Messages = Server.GetMessages(CurrentUser.ID, GetSelectedUserID());
@@ -109,19 +148,28 @@ namespace Client.Models
             return Messages;
         }
 
-        //Отправить сообщение
+        /// <summary>
+        /// Отправка сообщения
+        /// </summary>
+        /// <param name="message">Сообщение которое необходимо отправить</param>
         public void Message(string message)
         {
             Message Message = MessageFactory(message);
             if (!string.IsNullOrEmpty(message))
             {
+                //Обновить данные о последнем сообщении в списке пользователей
                 UpdateConnectedUsers(Message.ReceiverID, Message.SendDate);
+                //Отправить сообщение на сервер
                 Server.Message(Message);
                 AddMessageToList(Message);
             }
         }
 
-        //Фабрика сообщений
+        /// <summary>
+        /// Фабрика сообщений 
+        /// </summary>
+        /// <param name="message">Содержимое сообщения</param>
+        /// <returns>Экземпляр сообщения с установленными полями</returns>
         public Message MessageFactory(string message)
         {
             return new Message()
